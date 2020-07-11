fc977bfffe6410f74ca8c88fe8f65449a5907228
lambhunter0 <george.theophanous@hotmail.com>
2020-07-11 17:37:45 -0400

    police car, road segments, and organize scene

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public SpriteRenderer sr;
+    public float flickerLength;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        _currentFlickerTick -= Time.deltaTime;
+        if (_currentFlickerTick <= 0.0f) 
+        {
+            _currentFlickerTick = flickerLength;
+            SwapSprite();
+        }
+    }
+
+    private void SwapSprite() 
+    {
+        if (sr.sprite == sprites[0])
+        {
+            sr.sprite = sprites[1];
+        }
+        else 
+        {
+            sr.sprite = sprites[0];
+        }
+
+    }
+    private float _currentFlickerTick;
+}
